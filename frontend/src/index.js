61941fa978275d262fb6f21f6d02ea656fc62fef
Jared Luxenberg <jared@jaredlux.com>
2017-06-16 13:00:56 -0700

    add polyfills for IE support

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,3 +1,7 @@
+import 'core-js/fn/array/includes';
+import 'core-js/fn/array/find';
+import 'core-js/fn/string/starts-with';
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
