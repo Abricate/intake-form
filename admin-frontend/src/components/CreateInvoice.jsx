b441fd26c49d5834c8f754b1717ffecb5e89f342
Jared Luxenberg <jared@jaredlux.com>
2017-08-17 12:46:56 -0700

    fixing some bugs in the invoicing frontend

--- admin-frontend/src/components/CreateInvoice.jsx
+++ admin-frontend/src/components/CreateInvoice.jsx
@@ -9,7 +9,7 @@ import _ from 'lodash';
 
 import AbricateLogo from '../abricate-logo.png';
 import { getJobsWithIds } from '../api';
-import { fmtDollars, parseDollars } from '../util';
+import { fmtNumber, fmtDollars, parseDollars } from '../util';
 
 const phoneUtil = phonenumber.PhoneNumberUtil.getInstance();
 
@@ -37,9 +37,9 @@ const StaticAddress = ({ name, address1, address2, city, state, zipcode, phone }
     <address>
       <strong>{name}</strong><br/>
       {address1}<br/>
-      {address2 ? <span>{address2}<br/></span> : null}
-      {city}, {state} {zipcode}<br/>
-      {phoneNational ? <a href={`tel:${phoneInternational}`}>{phoneNational}</a> : null}
+    {address2 ? <span>{address2}<br/></span> : null}
+    {city}, {state} {zipcode}<br/>
+    {phoneNational ? <a href={`tel:${phoneInternational}`}>{phoneNational}</a> : null}
     </address>
   );
 }
@@ -77,82 +77,94 @@ const EditableAddress = ({ name, address1, address2, city, state, zipcode, phone
 
 const Address = Editable(StaticAddress, EditableAddress);
 
-const InvoiceTemplate = ({ total, shipping, date, customerAddress = {}, setCustomerAddressField, setShipping, editable, children }) => (
-<div className="mt-3">
-  <Row>
-    <Col xs="6">
-      <h1>Invoice</h1>
-      <Address name="Abricate, Inc." address1="12345 Foobar Lane" city="San Francisco" state="CA" zipcode="94117" phone="800-555-1234" />
-      <Address editable={editable} set={setCustomerAddressField} {...customerAddress}  />
-    </Col>
-    <Col xs="6">
-      <div className="text-right">
-        <img alt="Abricate" width="300" src={AbricateLogo} />
-      </div>
-      <Table bordered className="mt-5">
-        <tbody>
-          <tr>
-            <th>Invoice #</th>
-            <td>12341</td>
-          </tr>
-          <tr>
-            <th>Date</th>
-            <td>{date}</td>
-          </tr>
-          <tr>
-            <th>Amount Due</th>
-            <td>{fmtDollars(total)}</td>
-          </tr>
-        </tbody>
-      </Table>
-    </Col>
-  </Row>
-  <Table className="mt-5">
-    <thead>
-      <tr>
-        <th>Filename</th>
-        <th>Order Identifier</th>
-        <th>Material</th>
-        <th>Quantity</th>
-        <th className={editable ? null : 'text-right'}>Unit Price</th>
-        <th></th>
-      </tr>
-    </thead>
-    <tbody>
-      {children}
-      <tr>
-        <td></td><td></td><td></td><td></td>
-        <th>Shipping</th>
-        <td style={editable ? null : MoneyStyle}>
-          {editable ? (
-            <InputGroup>
-              <InputGroupAddon>$</InputGroupAddon>
-              <Input style={{width: '50px'}} value={shipping} onChange={setShipping} />
-            </InputGroup>
-          ) : (
-            fmtDollars(shipping)
-          )}
-        </td>
-      </tr>
-      <tr>
-        <td></td><td></td><td></td><td></td>
-        <th className="table-inverse">Total</th>
-        <td style={MoneyStyle} className="table-inverse">{fmtDollars(total)}</td>
-      </tr>
-    </tbody>
-  </Table>
-  <Row>
-    <Col xs={{size: 6, offset: 6}}>
-      
-      <Table>
-        <tbody>
-          <tr>
-          </tr>
-        </tbody>
-      </Table>
-    </Col>  
-  </Row>
-</div>
+const InvoiceTemplate = ({
+  customerAddress = {},
+  date,
+  shipping = {},
+  total,
+
+  setCustomerAddressField,
+  setShipping,
+  
+  editable,
+
+  children
+}) => (
+  <div className="mt-3">
+    <Row>
+      <Col xs="6">
+        <h1>Invoice</h1>
+        <Address name="Abricate, Inc." address1="12345 Foobar Lane" city="San Francisco" state="CA" zipcode="94117" phone="800-555-1234" />
+        <Address editable={editable} set={setCustomerAddressField} {...customerAddress}  />
+      </Col>
+      <Col xs="6">
+        <div className="text-right">
+          <img alt="Abricate" width="300" src={AbricateLogo} />
+        </div>
+        <Table bordered className="mt-5">
+          <tbody>
+            <tr>
+              <th>Invoice #</th>
+              <td>12341</td>
+            </tr>
+            <tr>
+              <th>Date</th>
+              <td>{date}</td>
+            </tr>
+            <tr>
+              <th>Amount Due</th>
+              <td>{fmtDollars(total)}</td>
+            </tr>
+          </tbody>
+        </Table>
+      </Col>
+    </Row>
+    <Table className="mt-5">
+      <thead>
+        <tr>
+          <th>Filename</th>
+          <th>Order Identifier</th>
+          <th>Material</th>
+          <th>Quantity</th>
+          <th className={editable ? null : 'text-right'}>Unit Price</th>
+          <th></th>
+        </tr>
+      </thead>
+      <tbody>
+        {children}
+        <tr>
+          <td></td><td></td><td></td><td></td>
+          <th>Shipping</th>
+          <td style={editable ? null : MoneyStyle}>
+            {editable ? (
+               <InputGroup>
+                 <InputGroupAddon>$</InputGroupAddon>
+                 <Input style={{width: '50px'}} value={shipping.str} onChange={setShipping} />
+               </InputGroup>
+            ) : (
+               fmtDollars(shipping.value)
+            )}
+          </td>
+        </tr>
+        <tr>
+          <td></td><td></td><td></td><td></td>
+          <th className="table-inverse">Total</th>
+          <td style={MoneyStyle} className="table-inverse">{fmtDollars(total)}</td>
+        </tr>
+      </tbody>
+    </Table>
+    <Row>
+      <Col xs={{size: 6, offset: 6}}>
+        
+        <Table>
+          <tbody>
+            <tr>
+            </tr>
+          </tbody>
+        </Table>
+      </Col>  
+    </Row>
+  </div>
 );
 
 const MoneyStyle = {
@@ -171,12 +183,12 @@ const InvoiceLineItem = ({ filename, note, orderIdentifier, material, quantity, 
       <td>{quantity}</td>
       <td style={editable ? null : MoneyStyle}>
         {editable ? (
-          <InputGroup>
-            <InputGroupAddon>$</InputGroupAddon>
-            <Input style={{width: '50px'}} value={unitPrice.str} onChange={setPrice} />
-          </InputGroup>
+           <InputGroup>
+             <InputGroupAddon>$</InputGroupAddon>
+             <Input style={{width: '50px'}} value={unitPrice.str} onChange={setPrice} />
+           </InputGroup>
         ) : (
-          fmtDollars(unitPrice.value)
+           fmtDollars(unitPrice.value)
         )}
       </td>
       <td style={MoneyStyle}>{unitPrice.value != null ? fmtDollars(unitPrice.value * quantity) : null}</td>
@@ -262,19 +274,36 @@ class CreateInvoice extends React.Component {
   }
 
   setShipping(event) {
-    const shipping = parseDollars(event.target.value);
-    this.setState({ shipping });
+    const str = event.target.value;
+    const value = parseDollars(str);
+
+    this.setState({ shipping: { str, value } });
   }
   
   jobs() {
     return this.props.match.params.jobIds.split(',').map( jobId => this.state.jobs[jobId] ).filter(job => job != null);
   }
-  
+
+  prices() {
+    const unchangedPrices = _.mapValues(
+      this.state.jobs,
+      (value, key) => ({ value: value.unitPrice, str: fmtNumber(value.unitPrice) })
+    );
+
+    
+    return {
+      ...unchangedPrices,
+      ...this.state.prices
+    };
+  }
+      
   render() {
+    if(_.isEmpty(this.state.jobs)) return null;
+    
     const jobs = this.jobs();
-    const subtotal = _.sum(_.map(this.state.prices, ({ value }, jobId) => value ? value * this.state.jobs[jobId].quantity : 0));
+    const subtotal = _.sum(_.map(this.prices(), ({ value }, jobId) => value ? (value * this.state.jobs[jobId].quantity) : 0));
     const shipping = this.state.shipping;
-    const total = subtotal + (shipping != undefined ? shipping : 0);
+    const total = subtotal + (shipping != undefined ? shipping.value : 0);
     const date = moment().format('LL');
 
     const isBlocking = !(_.isEmpty(this.state.prices) && _.isEmpty(this.state.comments) && _.isEmpty(this.state.customerAddress) && this.state.shipping == undefined);
--- admin-frontend/src/util.js
+++ admin-frontend/src/util.js
@@ -8,6 +8,14 @@ export function fmtDollars(number) {
   }
 }
 
+export function fmtNumber(number) {
+  if(isNaN(number)) {
+    return '';
+  } else {
+    return number.toLocaleString(undefined, {minimumFractionDigits: 2, maximumFractionDigits:2});
+  }
+}
+
 export function parseDollars(str) {
   const dollars = parseDecimalNumber(str);
   if(!isNaN(dollars)) {
